bd115cedee7a928c1e6c2cfd6baa549878fa1f74
James Giroux <info@transforme.ca>
2013-07-04 18:03:25 -0400

    James' attempt at the program.

--- beer.rb
+++ beer.rb
@@ -1,5 +1,16 @@
 class Beer
 	def song(bottles)
-		
+		origin = bottles
+		while bottles > 1
+			puts "#{bottles} bottles of beer on the wall. #{bottles} bottles of beer.  You take one down and pass it around #{bottles - 1} bottles of beer on the wall."
+			bottles = bottles - 1
+		end
+		if bottles == 1
+			puts "#{bottles} bottle of beer on the wall. #{bottles} bottle of beer.  You take one down and pass it around no bottles of beer on the wall."
+			puts "No bottles of beer on the wall. No more bottles of beer.  Go to the store and buy some more, #{origin} bottles of beer on the wall."
+			bottles = -1
+		end
 	end
 end
+
+song = Beer.new
